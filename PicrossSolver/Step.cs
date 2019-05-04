fac020c82ea707b1e9a1ebc0d689ba5a786282e5
codegrumps <kylelbauer@gmail.com>
2019-05-04 13:14:19 -0400

    Introduce Step class
    Steps represent the actions taken in solving a picross puzzle. No logic
    is currently present, only a few properties, an enum, and a prototype
    constructor.

---
+++ PicrossSolver/Step.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Step
+{
+
+	public Step(StepAction a)
+	{
+       
+	}
+
+    enum StepAction
+    {
+        OverlapFill, BackkFill, FrontFill, GapFill
+    }
+
+    public int TableIndex { get; }
+    public SegmentLayout Layout { get; }
+
+    public StepAction Action { get; }
+    public string Before { get; }
+    public string After { get; }
+}
